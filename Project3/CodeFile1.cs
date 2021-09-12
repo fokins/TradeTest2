167a339abf97b5e8d1c7a03b7ba603f4793ab203
fokins <sergejfokin829@gmail.com>
2021-09-12 13:29:17 +0300

    Добавьте файлы проекта.

---
+++ Project3/CodeFile1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+class Trade
+{
+    int Price;
+    Random rnd;
+
+    public void RandomPrice(int rand)
+    {
+        rnd = new Random(rand);
+        Price = rnd.Next(0, 100);
+    }
+
+    public int GetPrice()
+    {
+        return Price;
+    }
+}
+
+class Processing
+{
+    static Queue<int> Prices = new Queue<int>();
+
+    static void Main()
+    {
+        Thread Generator = new Thread(GenerateTrade);
+        Generator.Start();
+
+        while (true)
+        {
+            if(Prices.Count > 0)
+            {
+                Console.WriteLine(Prices.Dequeue());
+            }
+        }
+    }
+
+    static void GenerateTrade()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Trade t = new Trade();
+            t.RandomPrice(i);
+            Prices.Enqueue(t.GetPrice());
+            Thread.Sleep(1);
+        }
+    }
+}
+
